6bbbbcce8fa734077b44f37eeae05b4f4fae0d61
Israel Berlanga <israel0688@hotmail.com>
2020-07-01 00:02:15 -0500

    working routes again

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,56 +1,56 @@
-// sign up
-async function signupFormHandler(event) {
-    event.preventDefault();
+// // sign up
+// async function signupFormHandler(event) {
+//     event.preventDefault();
   
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+//     const name = document.querySelector('#name-signup').value.trim();
+//     const email = document.querySelector('#email-signup').value.trim();
+//     const password = document.querySelector('#password-signup').value.trim();
   
-    if (name && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          name,
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
+//     if (name && email && password) {
+//       const response = await fetch('/api/users', {
+//         method: 'post',
+//         body: JSON.stringify({
+//           name,
+//           email,
+//           password
+//         }),
+//         headers: { 'Content-Type': 'application/json' }
+//       });
   
-      // check the response status
-      if (response.ok) {
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+//       // check the response status
+//       if (response.ok) {
+//         console.log('success');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   }
   
-  document.querySelector('#signup-submit').addEventListener('submit', signupFormHandler);
+//   document.querySelector('#signup-submit').addEventListener('submit', signupFormHandler);
 
-//   login
-  async function loginFormHandler(event) {
-    event.preventDefault();
+// //   login
+//   async function loginFormHandler(event) {
+//     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+//     const email = document.querySelector('#email-login').value.trim();
+//     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
-        body: JSON.stringify({
-          email,
-          password
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
+//     if (email && password) {
+//       const response = await fetch('/api/users/login', {
+//         method: 'post',
+//         body: JSON.stringify({
+//           email,
+//           password
+//         }),
+//         headers: { 'Content-Type': 'application/json' }
+//       });
   
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  }
+//       if (response.ok) {
+//         document.location.replace('/');
+//       } else {
+//         alert(response.statusText);
+//       }
+//     }
+//   }
   
-  document.querySelector('#login-submit').addEventListener('submit', loginFormHandler);
+//   document.querySelector('#login-submit').addEventListener('submit', loginFormHandler);
